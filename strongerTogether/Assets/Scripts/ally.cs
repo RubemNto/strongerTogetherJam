63c11e6307d27b92b7b9c0a7c4897de823c07a51
rubem <rubemchrist@gmail.com>
2021-02-16 17:45:49 +0000

    .

---
+++ strongerTogether/Assets/Scripts/ally.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ally : MonoBehaviour
+{
+    public GameObject enemyTarget = null;
+    public enemiesManager enemiesManager;
+    public partyManager partyManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        partyManager = GameObject.Find("partyManager").GetComponent<partyManager>();
+        enemiesManager = GameObject.Find("enemiesManager").GetComponent<enemiesManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemyTarget == null)
+        {            
+            List<GameObject> allyTargets = new List<GameObject>();
+            List<GameObject> availableTargets = new List<GameObject>();
+            foreach (GameObject aly in partyManager.instantiatedPartyMember)
+            {
+                allyTargets.Add(aly.GetComponent<ally>().enemyTarget);
+            }
+
+            for (int i = 0; i < enemiesManager.enemiesOnScreen.Count; i++)
+            {
+                bool found = false;
+                for (int b = 0; b < allyTargets.Count; b++)
+                {
+                    if(enemiesManager.enemiesOnScreen[i] == allyTargets[b])
+                    {
+                        found = true;
+                        break;
+                    }else
+                    {
+                        found = false;
+                    }   
+                }
+                if(found == false)
+                {
+                    availableTargets.Add(enemiesManager.enemiesOnScreen[i]);
+                }                
+            }
+            
+            float[] distances = new float[availableTargets.Count];
+
+            for (int i = 0; i < distances.Length; i++)
+            {
+                distances[i] = Vector2.Distance(transform.position,availableTargets[i].transform.position);
+            }
+
+            float[] tempDistances = distances;
+            Array.Sort(distances);
+            for (int i = 0; i < distances.Length; i++)
+            {
+                if(distances[0] == tempDistances[i])
+                {
+                    enemyTarget = availableTargets[i];
+                    break;
+                }
+            }
+        }
+        
+    }
+}
---
+++ strongerTogether/Assets/Scripts/tankAlly.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class tankAlly : MonoBehaviour
+{
+    private ally ally;
+    [Header("Movement Attributes")]
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ally = GetComponent<ally>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ally.enemyTarget != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,ally.enemyTarget.transform.position,moveSpeed*Time.deltaTime);
+        }
+    }
+}
