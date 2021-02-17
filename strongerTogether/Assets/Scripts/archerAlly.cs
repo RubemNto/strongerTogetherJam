bce9d8043f24567cc9e82187799e52bf546c9d7f
rubem <rubemchrist@gmail.com>
2021-02-17 15:57:24 +0000

    .

---
+++ strongerTogether/Assets/Scripts/archerAlly.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class archerAlly : MonoBehaviour
+{
+    private ally ally;
+    private float timeOfShot;
+    public float timeBetweenShots;
+    public GameObject arrowPefab;
+    public Transform arrowSpawnPosition;
+    public float[] minMaxDamage = new float[2];
+    public float[] minMaxSpeed = new float[2];
+    // Start is called before the first frame update
+    void Start()
+    {
+        ally = GetComponent<ally>();
+        timeOfShot = timeBetweenShots;        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(ally.enemyTarget != null)
+        {
+            //shoot arrow
+            if(timeOfShot <= 0)
+            {
+                shootArrow();
+                timeOfShot=timeBetweenShots;
+            }else
+            {
+                timeOfShot-=Time.deltaTime;
+            }
+        }        
+    }
+    public void shootArrow()
+    {
+        GameObject tempArrow = Instantiate(arrowPefab,arrowSpawnPosition.position,arrowSpawnPosition.rotation);
+        //Set Arrow damage
+        tempArrow.GetComponent<arrow>().SetDamage(Random.Range(minMaxDamage[0],minMaxDamage[0]+1));
+        //Set Arrow speed
+        tempArrow.GetComponent<arrow>().SetSpeed(Random.Range(minMaxSpeed[0],minMaxSpeed[0]+1));
+        //Set Arrow target
+        tempArrow.GetComponent<arrow>().SetTarget(ally.enemyTarget);
+    }
+}
---
+++ strongerTogether/Assets/Scripts/arrow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    private GameObject target;
+    private float speed;
+    private float damage;
+    
+    private void Update()
+    {
+        transform.right = (target.transform.position - transform.position).normalized;
+        transform.position = Vector2.MoveTowards(transform.position,target.transform.position,speed*Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "enemy")
+        {
+            Destroy(other.gameObject);
+        }        
+    }
+
+    public void SetSpeed(float Speed)
+    {
+        speed = Speed;
+    }
+    public void SetDamage(float Damage)
+    {
+        damage = Damage;
+    }
+    public void SetTarget(GameObject Target)
+    {
+        target = Target;
+    }
+
+    
+}
